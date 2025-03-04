81aa4d2aa4b2a273293b7e7390feebfd2d75784d
Koushik Sarkar <sarkarkoushik557@gmail.com>
2025-03-05 01:17:21 +0530

    intial server setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { io } from 'socket.io-client';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const socket = io('http://localhost:5000');
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header>
+          <h1 className="text-center">Welcome to Simple Chat App</h1>
+        </header>
+        <main className="maincontainer">
+          <div id="chat-container">
+            <div className="mitem left">hi</div>
+            <div className="mitem right">hello</div>
+          </div>
+          <form id="chat-form">
+            <input type="text" id="message-input" placeholder="Type your message here..." required />
+            <button type="submit" className="sendbtn">Send</button>
+          </form>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-const socketio = require('socket.io')(8000);
+const express = require('express');
+const { createServer } = require('http');
+const cors = require('cors');
+const socketio = require('socket.io');
+
+
+const app = express();
+app.use(cors());
+
+const myserver = createServer(app);
+const io = new socketio.Server(myserver);
+
+
+io.on('connection', () => {
+    console.log('user connected');
+})
+
+
+app.get('/', (req, res) => {
+    res.send('Server is up and running');
+    console.log('base');
+})
+
+myserver.listen(9000, () => {
+    console.log('Server is running on port 9000');
+});
 
